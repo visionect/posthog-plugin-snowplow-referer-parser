78bb1f07a1ca6dd15708c10c627849da3d79d7fd
Miha Drofenik <miha.drofenik@visionect.com>
2023-07-27 10:42:53 +0200

    Fixed a bug causing problems with certain properties not being set and messing with the statistics

--- index.ts
+++ index.ts
@@ -73,6 +73,10 @@ function processEvent(event) {
         $set_once: {
           initial_source: "direct",
           initial_referrer_parser: "direct_and_own_domains",
+          initial_medium: "undefined",
+          initial_campaign: "undefined",
+          initial_content: "undefined",
+          initial_term: "undefined",
         },
       },
     }
@@ -109,6 +113,8 @@ function processEvent(event) {
         initial_source: referrerData.source.toLowerCase(),
         initial_term: searchQuery,
         initial_referrer_parser: "snowplow",
+        initial_campaign: "undefined",
+        initial_content: "undefined",
       },
     },
   }
